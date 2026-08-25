ac447dfb558f28334a76fabc0f113614d7f81eb8
TricksTok <107802038+TomasDelon@users.noreply.github.com>
2026-08-25 17:37:48 +0200

    Use build-time pack resolver in application bootstrap

--- algebre-src/src/main.jsx
+++ algebre-src/src/main.jsx
@@ -4,22 +4,14 @@ import 'mathlive/fonts.css';
 import './styles.css';
 import './trainer/trainer.css';
 import './trainer/progress.css';
-import activePack from '@active-trainer-pack';
+import { resolveBootPack } from '@trainer-pack-resolver';
 import { TrainerApp } from './trainer/TrainerApp.jsx';
 
 MathfieldElement.soundsDirectory=null;
 MathfieldElement.keypressVibration=false;
 
-async function resolveBootPack(){
-  if(__TRAINER_ALLOW_PACK_SWITCH__){
-    const {resolveTrainerPack}=await import('./packs/index.js');
-    return resolveTrainerPack(location.search,activePack.id);
-  }
-  return activePack;
-}
-
 async function boot(){
-  const pack=await resolveBootPack();
+  const pack=resolveBootPack();
   let debugMath=null;
   if(__TRAINER_ALLOW_PACK_SWITCH__||import.meta.env.DEV) debugMath=await import('./math.js');
 
